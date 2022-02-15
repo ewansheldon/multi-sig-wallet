aa2d9766bb3cb309bb5a3dcefa90daf212895fc4
ewansheldon <ewansheldon@gmail.com>
2022-02-15 19:53:04 +0000

    test transaction which has already been executed

--- test/multi-sig-wallet.js
+++ test/multi-sig-wallet.js
@@ -12,21 +12,36 @@ contract('MultiSigWallet', accounts => {
     wallet = await MultiSigWallet.new(owners, NUM_CONFIRMATIONS_REQUIRED);
   });
 
-  it('should execute transaction', async () => {
-    let to = owners[0];
-    let value = 0;
-    let data = "0x0";
-    await wallet.submitTransaction(to, value, data);
-    await wallet.confirmTransaction(0, {from: owners[0]});
-    await wallet.confirmTransaction(0, {from: owners[1]});
+  describe('executeTransaction', async () => {
+    beforeEach(async () => {
+      let to = owners[0];
+      let value = 0;
+      let data = "0x0";
+      await wallet.submitTransaction(to, value, data);
+      await wallet.confirmTransaction(0, {from: owners[0]});
+      await wallet.confirmTransaction(0, {from: owners[1]});
+    });
 
-    const result = await wallet.executeTransaction(0, {from: owners[0]});
-    const { logs } = result;
-    const transaction = await wallet.getTransaction(0);
+    it('should execute transaction', async () => {
+      const result = await wallet.executeTransaction(0, {from: owners[0]});
+      const {logs} = result;
+      const transaction = await wallet.getTransaction(0);
 
-    assert.equal(logs[0].event, "ExecuteTransaction");
-    assert.equal(logs[0].args.owner, owners[0]);
-    assert.equal(logs[0].args.txIndex, 0);
-    assert.equal(transaction.executed, true);
+      assert.equal(logs[0].event, "ExecuteTransaction");
+      assert.equal(logs[0].args.owner, owners[0]);
+      assert.equal(logs[0].args.txIndex, 0);
+      assert.equal(transaction.executed, true);
+    });
+
+    it('should not execute transaction, given already executed', async () => {
+      await wallet.executeTransaction(0, {from: owners[0]});
+
+      try {
+        await wallet.executeTransaction(0, {from: owners[0]});
+        throw new Error('transaction did not fail');
+      } catch(error) {
+        assert.equal(error.reason, 'tx already executed');
+      }
+    });
   });
 });
